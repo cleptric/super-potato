4277a961e0415e5f2fedebd9e07458f53922526a
Michael Hoffmann <michael@magicplan.app>
2021-05-25 01:32:23 +0200

    Add new seed and cleanup

---
+++ config/Seeds/aiports.sql
@@ -0,0 +1,66 @@
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'loww',
+    NOW(),
+    NOW()
+);
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'lowi',
+    NOW(),
+    NOW()
+);
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'lows',
+    NOW(),
+    NOW()
+);
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'lowg',
+    NOW(),
+    NOW()
+);
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'lowg',
+    NOW(),
+    NOW()
+);
+INSERT INTO `airports` (
+    `id`,
+    `name`,
+    `created`,
+    `modified`
+) VALUES (
+    UUID(),
+    'lowl',
+    NOW(),
+    NOW()
+);
