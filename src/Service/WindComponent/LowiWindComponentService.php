10bf12584d913b06b9c3612850e7973849b4dd63
Michael Hoffmann <michael@magicplan.app>
2021-06-04 06:59:27 +0200

    Fix LOWI wind components

--- src/Service/WindComponent/LowiWindComponentService.php
+++ src/Service/WindComponent/LowiWindComponentService.php
@@ -10,7 +10,7 @@ class LowiWindComponentService extends AbstractWindComponentService
      * @var array
      */
     protected array $_trueHeadings = [
-        '8' => 81,
+        '08' => 81,
         '26' => 261,
     ];
 }
