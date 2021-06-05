98e0431a41f9afdc6ea555422c9b631a750c5a83
Michael Hoffmann <michael@magicplan.app>
2021-06-05 09:59:37 +0200

    Improve wording

--- frontend/js/components/Onboarding.vue
+++ frontend/js/components/Onboarding.vue
@@ -134,7 +134,7 @@
                             </div>
                             <div>
                                 <p class="mt-12 max-w-prose mx-auto text-xl text-gray-500">
-                                    Be sure to head over to the Settings page to adjust them to your personal preference.
+                                    Be sure to head over to the Settings page to adjust the settings to your personal preferences.
                                 </p>
                                 <button
                                     class="mt-5 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
