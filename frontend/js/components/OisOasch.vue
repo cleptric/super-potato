7daee84026ae109d6d1b31e23300aa5e4a1f3d2f
Michael Hoffmann <michael@magicplan.app>
2021-06-18 01:28:34 +0200

    Change refresh button color

--- frontend/js/components/OisOasch.vue
+++ frontend/js/components/OisOasch.vue
@@ -23,13 +23,13 @@
     </nav>
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <h1 class="pt-[25%] mb-2 text-2xl text-center font-semibold">
-            Looks like the Super Potato is broken beyond repair
+            Looks like Super Potato is broken beyond repair
         </h1>
         <p class="mb-16 text-center text-sm text-gray-500 italic">
             Ois Oasch hoid...
         </p>
         <div class="text-center">
-            <a href="/" class="py-2 px-4 border border-blue-300 rounded-md shadow-sm text-center text-sm font-medium text-blue-600 bg-blue-50 hover:bg-blue-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <a href="/" class="bg-blue-600 border border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600">
                 Refresh page
             </a>
         </div>
