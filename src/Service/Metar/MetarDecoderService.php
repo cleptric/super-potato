4b09dc23c0b22f73c22fab97683300d691088eeb
Michael Hoffmann <michael@magicplan.app>
2021-06-20 04:56:22 +0200

    Add MET conditions

--- src/Service/Metar/MetarDecoderService.php
+++ src/Service/Metar/MetarDecoderService.php
@@ -53,11 +53,74 @@ class MetarDecoderService
             'wind_shear_runways' => !empty($this->_decoder->getWindshearRunways()) ? $this->_decoder->getWindshearRunways() : null,
             'wind_shear_all_runways' => !empty($this->_decoder->getWindshearAllRunways()) ? $this->_decoder->getWindshearAllRunways() : null,
             'raw_metar' => !empty($this->_decoder->getRawMetar()) ? $this->_decoder->getRawMetar() : null,
+            'condition' => $this->_getCondition(),
         ];  
     }
 
     public function setMetar(?string $metar): void
     {
         $this->_metar = $metar;
+    }
+
+    protected function _getCondition(): string
+    {
+        $clouds = $this->_decoder->getClouds();
+        $cloudLayer = [];
+        if (isset($clouds[0])) {
+            if ($clouds[0]->getAmount() === 'OVC' || $clouds[0]->getAmount() === 'BKN')
+            $cloudLayer = [
+                'type' => $clouds[0]->getAmount(),
+                'base' => $clouds[0]->getBaseHeight()->getValue(),
+            ];
+        }
+
+        $visiblityObj = $this->_decoder->getVisibility();
+        $visiblity = null;
+        if (!empty($visiblityObj)) {
+            $visiblity = $visiblityObj->getVisibility()->getValue();
+        }
+
+        $rvr = $this->_decoder->getRunwaysVisualRange();
+        $runwayRvr = [];
+        if (!empty($rvr)) {
+            foreach ($rvr as $rwy) {
+                if ($rwy->getVisualRange()->getValue() < 600) {
+                    $runwayRvr = [
+                        'runway' => $rwy->getRunway(),
+                        'rvr' => $rwy->getVisualRange()->getValue(),
+                    ];
+                }
+            }
+        }
+
+        // LVP 3
+        if (
+            // RVR smaller 350m AND
+            !empty($runwayRvr) && $runwayRvr['rvr'] < 350 &&
+            // Celing below 200ft
+            !empty($cloudLayer) && $cloudLayer['base'] < 200
+        ) {
+            return 'LVP 3';
+        }
+
+        // LVP 2
+        if (
+            // RVR smaller 600m OR
+            !empty($runwayRvr) && $runwayRvr['rvr'] < 600 ||
+            // Celing below 200ft
+            !empty($cloudLayer) && $cloudLayer['base'] < 200
+        ) {
+            return 'LVP 2';
+        }
+
+        // LVP 1
+        if (
+            // Celing below 200ft
+            !empty($cloudLayer) && $cloudLayer['base'] < 200
+        ) {
+            return 'LVP 1';
+        }
+
+        return 'VMC';
     }
 }
