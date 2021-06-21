5e64a154272b6647864eb25c3ff8d95ae5b73c9d
Michael Hoffmann <michael@magicplan.app>
2021-06-21 23:59:11 +0200

    Style

--- frontend/js/views/List.vue
+++ frontend/js/views/List.vue
@@ -55,7 +55,7 @@
                                         >
                                             <button
                                                 class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-                                                :class="{ 'bg-blue-600': notifications[icao], 'bg-gray-200': !notifications[icao] }"
+                                                :class="{ 'bg-blue-600': notifications[icao], 'bg-gray-300': !notifications[icao] }"
                                                 @click.stop="toggleNotification(icao)"
                                             >
                                                 <span
