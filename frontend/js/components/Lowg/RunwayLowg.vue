29643db100ebae1a6ecaecf7a6b894834ccb2760
Michael Hoffmann <michael@magicplan.app>
2021-06-24 01:35:41 +0200

    Fix LOWG DEP/ARR runway

--- frontend/js/components/Lowg/RunwayLowg.vue
+++ frontend/js/components/Lowg/RunwayLowg.vue
@@ -64,13 +64,13 @@
                     >
                         <div
                             v-if="lowg.atis.arrival_runway.includes('34C') && lowg.closed_runways.includes('16C/34C') === false"
-                            class="absolute right-[-40px] h-6 w-6 transform -rotate-90"
+                            class="absolute left-[-40px] h-6 w-6 transform rotate-90"
                         >
                             <i class="far fa-lg fa-arrow-to-top"></i>
                         </div>
                         <div
                             v-if="lowg.atis.depature_runway.includes('34C') && lowg.closed_runways.includes('16C/34C') === false"
-                            class="absolute left-[-40px] h-6 w-6 transform -rotate-90"
+                            class="absolute left-[90px] h-6 w-6 transform rotate-90"
                         >
                             <i class="far fa-lg fa-arrow-up"></i>
                         </div>
@@ -126,13 +126,13 @@
                     >
                         <div
                             v-if="lowg.atis.arrival_runway.includes('16C') && lowg.closed_runways.includes('16C/34C') === false"
-                            class="absolute left-[-40px] h-6 w-6 transform rotate-90"
+                            class="absolute right-[-40px] h-6 w-6 transform -rotate-90"
                         >
                             <i class="far fa-lg fa-arrow-to-top"></i>
                         </div>
                         <div
                             v-if="lowg.atis.depature_runway.includes('16C') && lowg.closed_runways.includes('16C/34C') === false"
-                            class="absolute left-[90px] h-6 w-6 transform rotate-90"
+                            class="absolute left-[-40px] h-6 w-6 transform -rotate-90"
                         >
                             <i class="far fa-lg fa-arrow-up"></i>
                         </div>
