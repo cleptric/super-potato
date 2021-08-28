238ded3eaaf6c2368d9ef1888cec2db70e76127a
Michael Hoffmann <michael@magicplan.app>
2021-08-28 05:09:49 +0200

    Add loading state

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -13,7 +13,17 @@
 </head>
 <body class="bg-gray-50 min-w-[1024px]">
 
-    <div id="app"></div>
+    <div id="app">
+        <!-- Content is replaced by the vue app -->
+        <div class="absolute inset-0 flex items-center justify-center">
+            <div class="text-center">
+                <a href="/" class="flex items-center text-2xl font-extrabold">
+                    Super <img class="h-7 w-7 ml-2" src="/img/potato.png">
+                </a>
+                <span class="text-gray-500 text-sm">Loading...</span>
+            </div>
+        </div>
+    </div>
 
     <script src="https://kit.fontawesome.com/c1157ccfdb.js" crossorigin="anonymous"></script>
     <?= $this->Vite->vite(); ?>
