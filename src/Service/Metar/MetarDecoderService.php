f7a1b91bdc200899b40cdfc697318b4490eda1da
Michael Hoffmann <michael@magicplan.app>
2021-08-28 04:39:09 +0200

    Add condition to default values

--- src/Service/Metar/MetarDecoderService.php
+++ src/Service/Metar/MetarDecoderService.php
@@ -35,6 +35,7 @@ class MetarDecoderService
                 'wind_shear_all_runways' => null,
                 'condition' => null,
                 'raw_metar' => null,
+                'condition' => null,
             ];
         }
 
