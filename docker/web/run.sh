e14c03ee6b643e9ca6314d4c056b4c90a33d0ede
Michael Hoffmann <michael@magicplan.app>
2022-05-29 07:56:24 +0200

    Fix docker

--- docker/web/run.sh
+++ docker/web/run.sh
@@ -3,4 +3,4 @@
 service apache2 start
 service php8.1-fpm start
 
-# exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
