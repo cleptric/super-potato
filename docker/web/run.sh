40ce4776f6117ee95f08a4ca07d3bdd6fdf27fbc
Michael Hoffmann <michael@magicplan.app>
2022-05-29 06:39:17 +0200

    WIP Docker

--- docker/web/run.sh
+++ docker/web/run.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# wait until db is ready
-# https://selahattinunlu.medium.com/how-to-wait-for-a-container-to-be-ready-757bc1a86468
-# bin/cake migrations migrate
-
 service apache2 start
 service php8.1-fpm start
 
-/bin/sh -c bash
-
-# Important: This keeps the docker instance alive
-exec supervisord -n
+exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
