9be994b200adf5b874e6107a4ac667f904ca332a
Michael Hoffmann <michael@magicplan.app>
2022-06-20 08:28:32 +0200

    CS

--- src/Model/Table/OrganizationsTable.php
+++ src/Model/Table/OrganizationsTable.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
-use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
 
@@ -13,7 +11,6 @@ use Cake\Validation\Validator;
  *
  * @property \App\Model\Table\AirportsTable&\Cake\ORM\Association\HasMany $Airports
  * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsToMany $Users
- *
  * @method \App\Model\Entity\Organization newEmptyEntity()
  * @method \App\Model\Entity\Organization newEntity(array $data, array $options = [])
  * @method \App\Model\Entity\Organization[] newEntities(array $data, array $options = [])
@@ -27,7 +24,6 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\Organization[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
  * @method \App\Model\Entity\Organization[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
  * @method \App\Model\Entity\Organization[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
- *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
  */
 class OrganizationsTable extends Table
--- src/Model/Table/OrganizationsUsersTable.php
+++ src/Model/Table/OrganizationsUsersTable.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
-use Cake\ORM\Query;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
@@ -13,7 +12,6 @@ use Cake\Validation\Validator;
  *
  * @property \App\Model\Table\OrganizationsTable&\Cake\ORM\Association\BelongsTo $Organizations
  * @property \App\Model\Table\UsersTable&\Cake\ORM\Association\BelongsTo $Users
- *
  * @method \App\Model\Entity\OrganizationsUser newEmptyEntity()
  * @method \App\Model\Entity\OrganizationsUser newEntity(array $data, array $options = [])
  * @method \App\Model\Entity\OrganizationsUser[] newEntities(array $data, array $options = [])
@@ -27,7 +25,6 @@ use Cake\Validation\Validator;
  * @method \App\Model\Entity\OrganizationsUser[]|\Cake\Datasource\ResultSetInterface saveManyOrFail(iterable $entities, $options = [])
  * @method \App\Model\Entity\OrganizationsUser[]|\Cake\Datasource\ResultSetInterface|false deleteMany(iterable $entities, $options = [])
  * @method \App\Model\Entity\OrganizationsUser[]|\Cake\Datasource\ResultSetInterface deleteManyOrFail(iterable $entities, $options = [])
- *
  * @mixin \Cake\ORM\Behavior\TimestampBehavior
  */
 class OrganizationsUsersTable extends Table
